adccbe170d025c7b3d917bdc61c6083298e96f27
unknown <sabbir.cse@yahoo.com>
2021-02-24 20:27:01 +0600

    Welcome component

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <Welcome></Welcome>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+    </div>
+  );
+}
+
+function Welcome(){
+  return (
+    <div>
+      <h1>Welcome to React World !</h1>
     </div>
   );
 }
